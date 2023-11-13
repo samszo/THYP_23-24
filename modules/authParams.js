23f8ed23837454481637262282c88f1fc10386e5
Samuel Szoniecky <samszon@gmail.com>
2023-11-13 10:52:30 +0100

    Update authParams.js

--- modules/authParams.js
+++ modules/authParams.js
@@ -2,7 +2,7 @@ export const pa = {
     'navbar':d3.select('#navbarMain'),
     mail:'samuel.szoniecky@univ-paris8.fr',
     apiOmk:'../omk_thyp_23-24/api',
-    ident: 'HeDAW8W1fXG0HeBCyYOdjjP5NAyK6WNB',
-    key: '7sqEYiJMmLOSmbd1OCx7CAmzJbaHXY1j',
-    keyGitHub:"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"        
+    ident: '',
+    key: '',
+    keyGitHub:""        
 }
