e5475125ba88d9a887d597d3d6be75410f711c84
Samuel Szoniecky <samszon@gmail.com>
2023-11-13 10:49:45 +0100

    Add files via upload

---
+++ modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'samuel.szoniecky@univ-paris8.fr',
+    apiOmk:'../omk_thyp_23-24/api',
+    ident: '',
+    key: '',
+    keyGitHub:""        
+}
